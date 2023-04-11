93c10e2a3b83d35e9887121ca61050c33bf5ec46
vsubramaniasivam <vishal.subramaniasivam@alten.nl>
2023-04-11 11:24:56 +0200

    Compiled practice with every flag

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 
-
 int main(){
-	std::cout<<"Hello World"<<std::endl;
+
+	int* ptr {new int {10}};
+
+	if (ptr){
+		std::cout<<"Memory cated sucessfully"<<std::endl;
+	}
+
+	std::cout<<" The new number is "<< *ptr;
+
+
+	delete ptr;
+	ptr = nullptr;
+
+	if (ptr){
+		std::cout<<"ptr is not null"<<std::endl;
+	}
+	else if (!ptr){
+		std::cout<<"ptr is null"<<std::endl;
+	}
+
 	return 0;
 }
